8649d6d4273f6ff6054a614bbfa09a89c35efdc6
Mohammed Almostafa <58958280+csmohamme@users.noreply.github.com>
2022-12-30 23:39:58 -0800

    add validation to DTOs classes & create login function

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -34,9 +34,30 @@ namespace API.Controllers
             return user;
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+
+            if (user == null) return Unauthorized("Invalid username");
+
+            using var hmc = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmc.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+            }
+
+            return user;
+        }
+
+        // check if the username exists in the database or not
         private async Task<bool> UserExists(string username)
         {
             return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+
         }
     }
 }
--- API/DTOs/LoginDto.cs
+++ API/DTOs/LoginDto.cs
@@ -1,8 +1,12 @@
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace API.DTOs
 {
-    public class RegisterDto
+    public class LoginDto
     {
         [Required]
         public string Username { get; set; }
